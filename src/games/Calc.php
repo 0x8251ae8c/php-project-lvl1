7de7e73b0a2992ac4fb41e3dc3e47c6d18ba4803
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2022-03-17 23:06:15 +0700

    Add src/games/Calc.php

---
+++ src/games/Calc.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Php\Project\Lvl1\Games\Calc;
+
+use Php\Project\Lvl1\Engine;
+
+function play()
+{
+    $rules = 'What is the result of the expression?';
+    $operators = ['+', '-', '*'];
+
+    for ($i = 0; $i < Engine\NUM_ROUNDS; $i++) {
+        $operator = $operators[array_rand($operators)];
+        $number1 = rand(1, 10);
+        $number2 = rand(1, 10);
+
+        $questions[$i] = "Question: {$number1} {$operator} {$number2}";
+        
+        switch ($operator) {
+            case '+':
+                $answers[$i] = $number1 + $number2;
+                break;
+            case '-':
+                $answers[$i] = $number1 - $number2;
+                break;
+            case '*':
+                $answers[$i] = $number1 * $number2;
+                break;
+        }
+    }
+
+    $gameData[0] = $rules;
+    $gameData[1] = $questions;
+    $gameData[2] = $answers;
+
+    Engine\run($gameData);
+}
