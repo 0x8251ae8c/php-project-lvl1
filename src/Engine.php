2723a7ba9bdba74a5277f3c2c78925beaae01390
Alexander <99046156+0x8251ae8c@users.noreply.github.com>
2023-07-31 17:04:55 +0700

    Update Engine.php
    Fix Hexlet project check errors

--- src/Engine.php
+++ src/Engine.php
@@ -9,7 +9,7 @@ const NUM_OF_ROWS = 3;
 
 function run($goalOfTheGame, $questions, $answers): void
 {
-    line("Welcome to the Brain Games!");
+    line("Welcome to the Brain Game!");
     $name = prompt("May I have your name?");
     line("Hello, %s!", $name);
 
