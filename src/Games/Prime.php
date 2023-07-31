4e05bd6cb3914d4ef00443e5f965b8f7ffbf2afd
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2023-07-31 16:37:31 +0700

    Add game brain-prime

---
+++ src/Games/Prime.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Php\Project\Lvl1\Games\Prime;
+
+use Php\Project\Lvl1\Engine;
+
+function isPrimeNumber($number): bool
+{
+    if ($number < 2) {
+        return false;
+    }
+
+    for ($i = 2; $i < $number / 2; $i += 1) {
+        if ($number % $i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function play(): void
+{
+    $goalOfTheGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    $gameQuestions = [];
+    $rightAnswers = [];
+
+    for ($i = 0; $i < Engine\NUM_OF_ROWS; $i += 1) {
+        $number = random_int(1, 100);
+        $gameQuestions[] = "{$number}";
+        $rightAnswers[] = isPrimeNumber($number) ? 'yes' : 'no';
+    }
+
+    Engine\run($goalOfTheGame, $gameQuestions, $rightAnswers);
+}
